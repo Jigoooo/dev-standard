f934ab301e067beadd40d3ff4b2f7738cf9bca0d
Jigoooo <jiwook97@gmail.com>
2025-02-26 23:30:30 +0900

    폴더구조 변경

--- src/pages/home/ui/home.tsx
+++ src/pages/home/ui/home.tsx
@@ -2,60 +2,26 @@ import { useState } from 'react';
 
 import { useMenuState } from '@/entities/menu';
 import { MainLayout } from '@/entities/main';
-import { FlexRow, Select, Button, useModal, ModalLayout } from '@/shared/components';
+import { Select, FlexColumn, DatePicker, DateFromToPicker } from '@/shared/components';
 
 export function Home() {
   const menuState = useMenuState();
 
   const [test, setTest] = useState(1);
 
-  const trainerRegisterModal = useModal();
-
-  const testModalOpen = () => {
-    trainerRegisterModal.open(({ close }) => {
-      return (
-        <ModalLayout title={'테스트모달'} close={close}>
-          <FlexRow style={{ flexWrap: 'nowrap' }}>
-            <div style={{ whiteSpace: 'nowrap' }}>
-              테스트테스트테스트테스트테스트테스트테스트테스트테스트
-            </div>
-            <div style={{ whiteSpace: 'nowrap' }}>
-              테스트테스트테스트테스트테스트테스트테스트테스트테스트
-            </div>
-            <div style={{ whiteSpace: 'nowrap' }}>
-              테스트테스트테스트테스트테스트테스트테스트테스트테스트
-            </div>
-            <div style={{ whiteSpace: 'nowrap' }}>
-              테스트테스트테스트테스트테스트테스트테스트테스트테스트
-            </div>
-            <div style={{ whiteSpace: 'nowrap' }}>
-              테스트테스트테스트테스트테스트테스트테스트테스트테스트
-            </div>
-          </FlexRow>
-          <span>테스트</span>
-          <span>테스트</span>
-          <span>테스트</span>
-          <span>테스트</span>
-          <span>테스트</span>
-          <span>테스트</span>
-          <span>테스트</span>
-          <span>테스트</span>
-          <span>테스트</span>
-          <span>테스트</span>
-          <span>테스트</span>
-          <span>테스트</span>
-          <span>테스트</span>
-          <span>테스트</span>
-        </ModalLayout>
-      );
-    });
-  };
-
   return (
     <MainLayout headerTitle={menuState.selectedMenu.name}>
-      <FlexRow
-        style={{ width: '100%', height: '100%', alignItems: 'center', justifyContent: 'center' }}
+      <FlexColumn
+        style={{
+          width: '100%',
+          height: '100%',
+          alignItems: 'center',
+          justifyContent: 'center',
+          gap: 16,
+        }}
       >
+        <DatePicker />
+        <DateFromToPicker />
         <Select
           label={'드롭다운'}
           value={test}
@@ -66,8 +32,7 @@ export function Home() {
             { label: '3', value: 3 },
           ]}
         />
-        <Button onClick={testModalOpen}>test</Button>
-      </FlexRow>
+      </FlexColumn>
     </MainLayout>
   );
 }
