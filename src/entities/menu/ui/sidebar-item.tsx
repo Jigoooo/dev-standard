d074e5e9559637de9441bdf099f2b0ad0111e169
Jigoooo <jiwook97@gmail.com>
2025-03-04 21:27:49 +0900

    컴파일러 변경

--- src/entities/menu/ui/sidebar-item.tsx
+++ src/entities/menu/ui/sidebar-item.tsx
@@ -15,49 +15,55 @@ export function SidebarItem({
   const menuState = useMenuState();
 
   return (
-    <FlexRow
-      as={motion.div}
-      style={{
-        width: '100%',
-        gap: 12,
-        alignItems: 'center',
-        paddingBlock: 4,
-        paddingInline: 14,
-        cursor: 'pointer',
-        color: isSelected ? '#4ba7ff' : '#999999',
-        transition: 'all 0.1s',
-        minHeight: 42,
-        height: 42,
-        maxHeight: 42,
-      }}
-      whileHover={{
-        backgroundColor: 'rgba(255, 255, 255, 0.14)',
-        color: '#ffffff',
-      }}
-      transition={{
-        duration: 0.1,
-      }}
-      onClick={() => {
-        if (isSelected) return;
-
-        menuActions.setSelectedMenu(menu);
-        onClickMenu(menu);
-      }}
+    <Tooltip
+      style={{ backgroundColor: 'rgba(0, 0, 0, 0.5)' }}
+      position={'right'}
+      content={menu.name}
+      disabled={!menuState.sidebarCollapsed}
     >
-      <Tooltip position={'right'} content={menu.name} disabled={!menuState.delayedSidebarCollapsed}>
+      <FlexRow
+        as={motion.div}
+        style={{
+          width: '100%',
+          gap: 12,
+          alignItems: 'center',
+          paddingBlock: 4,
+          paddingInline: 14,
+          cursor: 'pointer',
+          color: isSelected ? '#4ba7ff' : '#999999',
+          transition: 'all 0.1s',
+          minHeight: 42,
+          height: 42,
+          maxHeight: 42,
+        }}
+        whileHover={{
+          backgroundColor: 'rgba(255, 255, 255, 0.14)',
+          color: '#ffffff',
+        }}
+        transition={{
+          duration: 0.1,
+        }}
+        onClick={() => {
+          if (isSelected) return;
+
+          menuActions.setSelectedMenu(menu);
+          onClickMenu(menu);
+        }}
+      >
         <menu.icon style={{ color: 'inherit', fontSize: '1.2rem' }} />
-      </Tooltip>
-      {!menuState.delayedSidebarCollapsed && (
-        <motion.span
-          initial={{ opacity: 0 }}
-          animate={{ opacity: 1 }}
-          exit={{ opacity: 0 }}
-          transition={{ duration: 0.2 }}
-          style={{ color: 'inherit', fontSize: '0.84rem', fontWeight: 700 }}
-        >
-          {menu.name}
-        </motion.span>
-      )}
-    </FlexRow>
+
+        {!menuState.delayedSidebarCollapsed && (
+          <motion.span
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            exit={{ opacity: 0 }}
+            transition={{ duration: 0.2 }}
+            style={{ color: 'inherit', fontSize: '0.84rem', fontWeight: 700 }}
+          >
+            {menu.name}
+          </motion.span>
+        )}
+      </FlexRow>
+    </Tooltip>
   );
 }
--- src/shared/components/view/ui/tooltip.tsx
+++ src/shared/components/view/ui/tooltip.tsx
@@ -14,6 +14,7 @@ type TooltipPosition =
   | 'right';
 
 type TooltipProps = {
+  style?: CSSProperties;
   position: TooltipPosition;
   children: ReactNode;
   content: ReactNode;
@@ -22,16 +23,16 @@ type TooltipProps = {
 
 const tooltipPositionStyles: Record<TooltipPosition, CSSProperties> = {
   top: { bottom: '100%', left: '50%', transform: 'translateX(-50%)' },
-  topLeft: { bottom: '100%', left: 0 },
-  topRight: { bottom: '100%', right: 0 },
+  topLeft: { bottom: '100%', right: '100%' },
+  topRight: { bottom: '100%', left: '100%' },
   bottom: { top: '100%', left: '50%', transform: 'translateX(-50%)' },
   bottomLeft: { top: '100%', left: 0 },
   bottomRight: { top: '100%', right: 0 },
-  left: { right: '100%', top: '50%', transform: 'translateY(-50%)' },
-  right: { left: '100%', top: '50%', transform: 'translateY(-50%)' },
+  left: { right: '100%', top: 0, transform: 'translateY(-50%)' },
+  right: { left: '100%', top: 0, transform: 'translateY(-50%)' },
 };
 
-export function Tooltip({ position, children, content, disabled = false }: TooltipProps) {
+export function Tooltip({ style, position, children, content, disabled = false }: TooltipProps) {
   const [isHovered, setIsHovered] = useState(false);
 
   return (
@@ -50,15 +51,18 @@ export function Tooltip({ position, children, content, disabled = false }: Toolt
             exit={{ opacity: 0, scale: 0.8 }}
             transition={{ duration: 0.2 }}
             style={{
-              position: 'absolute',
-              ...tooltipPositionStyles[position],
-              padding: 8,
-              backgroundColor: '#414141',
-              color: 'white',
-              borderRadius: 4,
-              whiteSpace: 'nowrap',
-              zIndex: TOOLTIP_Z_INDEX,
-              margin: 4,
+              ...{
+                position: 'absolute',
+                ...tooltipPositionStyles[position],
+                padding: 12,
+                backgroundColor: '#414141',
+                color: 'white',
+                borderRadius: 4,
+                whiteSpace: 'nowrap',
+                zIndex: TOOLTIP_Z_INDEX,
+                margin: 4,
+              },
+              ...style,
             }}
           >
             {content}
