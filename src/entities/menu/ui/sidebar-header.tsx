a3e656c9f0c6b99c26438c2e1d1e34900a076a11
Jigoooo <jiwook97@gmail.com>
2025-03-04 20:48:39 +0900

    컴파일러 변경

--- src/entities/menu/ui/sidebar-header.tsx
+++ src/entities/menu/ui/sidebar-header.tsx
@@ -1,13 +1,24 @@
 import { motion } from 'framer-motion';
 
-import { useMenuState } from '@/entities/menu';
+import { TbLayoutSidebarLeftCollapseFilled } from 'react-icons/tb';
+import { TbLayoutSidebarRightCollapseFilled } from 'react-icons/tb';
+
+import { menuActions, useMenuState } from '@/entities/menu';
 import { FlexRow } from '@/shared/components';
 
 export function SidebarHeader({ title }: { title: string }) {
   const menuState = useMenuState();
 
   return (
-    <FlexRow style={{ alignItems: 'center', paddingInline: 10, paddingBlock: 4, height: 42 }}>
+    <FlexRow
+      style={{
+        alignItems: 'center',
+        justifyContent: 'space-between',
+        paddingInline: 12,
+        paddingBlock: 4,
+        height: 42,
+      }}
+    >
       {!menuState.delayedSidebarCollapsed && (
         <motion.span
           initial={{ opacity: 0 }}
@@ -19,6 +30,22 @@ export function SidebarHeader({ title }: { title: string }) {
           {title}
         </motion.span>
       )}
+
+      <FlexRow
+        style={{
+          flexGrow: menuState.sidebarCollapsed ? 1 : 0,
+          alignItems: 'center',
+          justifyContent: 'center',
+          cursor: 'pointer',
+        }}
+        onClick={menuActions.toggleSidebarCollapsed}
+      >
+        {menuState.sidebarCollapsed ? (
+          <TbLayoutSidebarRightCollapseFilled style={{ fontSize: '1.2rem', color: '#ffffff' }} />
+        ) : (
+          <TbLayoutSidebarLeftCollapseFilled style={{ fontSize: '1.2rem', color: '#ffffff' }} />
+        )}
+      </FlexRow>
     </FlexRow>
   );
 }
--- src/entities/menu/ui/sidebar.tsx
+++ src/entities/menu/ui/sidebar.tsx
@@ -1,12 +1,8 @@
 import { motion } from 'framer-motion';
 
-import { TbLayoutSidebarLeftCollapseFilled } from 'react-icons/tb';
-import { TbLayoutSidebarRightCollapseFilled } from 'react-icons/tb';
-
 import { FlexColumn } from '@/shared/components';
 import {
   MainMenuController,
-  menuActions,
   menus,
   SidebarHeader,
   SidebarItems,
@@ -38,28 +34,6 @@ export function Sidebar({ headerTitle }: { headerTitle: string }) {
         <SidebarHeader title={headerTitle} />
         <SidebarItems menus={menus} />
       </FlexColumn>
-
-      <motion.button
-        animate={{ left: menuState.sidebarWidth }}
-        onClick={menuActions.toggleSidebarCollapsed}
-        style={{
-          position: 'absolute',
-          top: '40%',
-          zIndex: 10,
-          padding: '4px 8px',
-          border: 'none',
-          borderRadius: 4,
-          backgroundColor: '#1e232e',
-          color: '#fff',
-          cursor: 'pointer',
-        }}
-      >
-        {menuState.sidebarCollapsed ? (
-          <TbLayoutSidebarRightCollapseFilled />
-        ) : (
-          <TbLayoutSidebarLeftCollapseFilled />
-        )}
-      </motion.button>
 
       <MainMenuController />
     </motion.div>
