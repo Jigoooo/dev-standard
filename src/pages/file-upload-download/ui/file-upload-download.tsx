93618e8e5bb6062b8ac483078ac96cd32cb5341b
Jigoooo <jiwook97@gmail.com>
2025-03-06 23:08:33 +0900

    file 추가

--- src/pages/file-upload-download/ui/file-upload-download.tsx
+++ src/pages/file-upload-download/ui/file-upload-download.tsx
@@ -1,17 +1,30 @@
-import { FlexColumn, Typography } from '@/shared/components';
+import { FlexColumn, FlexRow } from '@/shared/components';
+import { FileUploadForm } from '@/shared/components/file/ui/file-upload-form.tsx';
 
 export function FileUploadDownload() {
   return (
     <FlexColumn
       style={{
         height: '100%',
-        gap: 16,
-        paddingBlock: 32,
-        paddingInline: 32,
+        paddingBlock: 16,
+        paddingInline: 16,
         overflowY: 'auto',
+        alignItems: 'center',
+        justifyContent: 'center',
       }}
     >
-      <Typography></Typography>
+      <FlexRow
+        style={{ border: '1px solid #cccccc', paddingBlock: 32, paddingInline: 16, width: 800 }}
+      >
+        <FileUploadForm
+          attachments={[]}
+          fileHandlerService={(file) => {
+            console.log(file);
+            return Promise.resolve({ path: '', idx: -1 });
+          }}
+          fileDelete={() => {}}
+        />
+      </FlexRow>
     </FlexColumn>
   );
 }
---
+++ src/shared/components/file/ui/drop-zone.tsx
@@ -0,0 +1,133 @@
+import React, { ReactNode, useRef, useState } from 'react';
+
+import FileUploadRoundedIcon from '@mui/icons-material/FileUploadRounded';
+
+import { isExtensionNotAllowed } from '@/shared/lib';
+import { colors } from '@/shared/constants';
+import { dialogActions, DialogType, FlexRow, Link, Typography } from '@/shared/components';
+
+export function DropZone({
+  handleFileList,
+  dropCautionContent,
+}: {
+  handleFileList: (file: File) => void;
+  dropCautionContent?: ReactNode;
+}) {
+  const inputRef = useRef<HTMLInputElement>(null);
+  const [fileUploadLoading, setFileUploadLoading] = useState<boolean>(false);
+
+  const fileHandler = async (file: File) => {
+    if (isExtensionNotAllowed(file.name)) {
+      dialogActions.openDialog({
+        dialogType: DialogType.ERROR,
+        title: '업로드 실패',
+        contents: '허용되지 않는 파일이에요',
+      });
+
+      return;
+    }
+
+    setFileUploadLoading(true);
+    handleFileList(file);
+    setFileUploadLoading(false);
+  };
+
+  const fileInputChange = async (event: React.ChangeEvent<HTMLInputElement>) => {
+    if (!event.target?.files?.[0]) return;
+
+    await fileHandler(event.target?.files?.[0]);
+    event.target.value = '';
+  };
+
+  const [dragOver, setDragOver] = useState<boolean>(false);
+  const relatedTargetRef = useRef<HTMLElement | null>(null);
+
+  const fileDropChange = async (event: React.DragEvent<HTMLInputElement>) => {
+    event.preventDefault();
+    setDragOver(false);
+    if (!event.dataTransfer?.files?.[0]) return;
+
+    await fileHandler(event.dataTransfer?.files?.[0]);
+  };
+
+  const setEnterDragWithBoundary = (event: React.DragEvent<HTMLDivElement>, value: boolean) => {
+    const relatedTarget =
+      event.relatedTarget ||
+      (value
+        ? relatedTargetRef.current
+        : window.document.elementFromPoint(event.clientX, event.clientY));
+
+    if (relatedTarget instanceof HTMLElement && event.currentTarget.contains(relatedTarget)) {
+      return;
+    }
+
+    relatedTargetRef.current = null;
+    setDragOver(value);
+  };
+
+  const handleDragOver = (event: React.DragEvent<HTMLLabelElement>) => {
+    event.preventDefault();
+    event.stopPropagation();
+  };
+
+  return (
+    <FlexRow
+      style={{
+        ...{
+          boxSizing: 'border-box',
+          borderRadius: 8,
+          border: dragOver ? `3px dashed ${colors.primary[500]}` : '3px dashed #bbbbbb',
+          transition: '0.2s',
+          display: 'flex',
+          flexDirection: 'column',
+          gap: 8,
+          alignItems: 'center',
+          justifyContent: 'center',
+          paddingInline: 24,
+          flexGrow: 1,
+          boxShadow: 'none',
+          minHeight: 115,
+          backgroundColor: dragOver ? colors.primary[100] : undefined,
+        },
+      }}
+      onDrop={fileDropChange}
+      onDragEnter={(event: React.DragEvent<HTMLDivElement>) =>
+        setEnterDragWithBoundary(event, true)
+      }
+      onDragLeave={(event: React.DragEvent<HTMLDivElement>) => {
+        if (event.target instanceof HTMLElement) {
+          relatedTargetRef.current = event.target;
+        }
+        setEnterDragWithBoundary(event, false);
+      }}
+      onDragOver={handleDragOver}
+    >
+      {!fileUploadLoading ? (
+        <>
+          <FlexRow
+            style={{
+              alignItems: 'center',
+              justifyContent: 'center',
+              padding: 12,
+              borderRadius: '50%',
+              backgroundColor: '#dddddd',
+            }}
+          >
+            <FileUploadRoundedIcon />
+          </FlexRow>
+          <Typography>
+            <Link onClick={() => inputRef.current && inputRef.current.click()}>
+              클릭 또는 드래그하여 파일 업로드
+            </Link>
+            <br />
+            {dropCautionContent && dropCautionContent}
+          </Typography>
+        </>
+      ) : (
+        // <CircularProgress />
+        <></>
+      )}
+      <input ref={inputRef} type={'file'} style={{ display: 'none' }} onChange={fileInputChange} />
+    </FlexRow>
+  );
+}
