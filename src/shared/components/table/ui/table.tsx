2b0fab79cb027072548c23b7e8815173d621c93e
Jigoooo <jiwook97@gmail.com>
2025-03-09 21:58:07 +0900

    파일 업로드

--- src/shared/components/table/ui/table.tsx
+++ src/shared/components/table/ui/table.tsx
@@ -45,8 +45,8 @@ const defaultTableStyle: TTableStyle = {
 };
 
 const TableContext = createContext<TTableContext<any> | null>(null);
-export const useTableContext = <T extends Record<string, any>>() => {
-  const tableContext = use(TableContext as Usable<TTableContext<T>>);
+export const useTableContext = <TData extends Record<string, any>>() => {
+  const tableContext = use(TableContext as Usable<TTableContext<TData>>);
 
   if (tableContext === null) {
     throw new Error('Table context is null. Please check the Table context ');
