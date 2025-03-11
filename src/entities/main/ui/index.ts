79881dc457f9ed42dbd938583378cfabdcde11a2
Jigoooo <jiwook97@gmail.com>
2025-03-11 09:34:12 +0900

    파일 업로드

--- src/entities/main/ui/index.ts
+++ src/entities/main/ui/index.ts
@@ -1,5 +1,4 @@
 export { MainLayout } from './main-layout.tsx';
 export { MainHeader } from './main-header.tsx';
 export { NoData } from './no-data.tsx';
-export { PageBackWithHeader } from './page-back-with-header.tsx';
 export { KeepAliveWrapper } from './keep-alive-wrapper.tsx';
