f1a0d5b2acaee6f2f88a9335534901039729fc27
Jigoooo <jiwook97@gmail.com>
2025-03-12 12:16:59 +0900

    동적 라우팅 수정

--- src/entities/router/ui/sidebar-items.tsx
+++ src/entities/router/ui/sidebar-items.tsx
@@ -1,7 +1,7 @@
 import { useLocation, useNavigate } from 'react-router-dom';
 
 import { SidebarItem } from './sidebar-item.tsx';
-import { FlexColumn } from '@/shared/components';
+import { Divider, FlexColumn } from '@/shared/components';
 import { TMenu } from '@/entities/router';
 
 export function SidebarItems({ menus }: { menus: TMenu[] }) {
@@ -16,6 +16,14 @@ export function SidebarItems({ menus }: { menus: TMenu[] }) {
     navigate(menu.router);
   };
 
+  const onClickSecondDepthMenu = (parentMenu: TMenu, childMenu: TMenu) => {
+    if (childMenu.isHeader) {
+      return;
+    }
+
+    navigate(`${parentMenu.router}/${childMenu.router}`);
+  };
+
   return (
     <FlexColumn style={{ width: '100%', alignItems: 'center' }}>
       {menus.map((menu, menuIndex) => {
@@ -24,13 +32,38 @@ export function SidebarItems({ menus }: { menus: TMenu[] }) {
         }
 
         const isSelected = menu.router ? location.pathname.includes(menu.router) : false;
+        const secondDepthMenus = menu.children;
 
         return (
           <FlexColumn style={{ width: '100%' }} key={menu.router}>
             <SidebarItem isSelected={isSelected} menu={menu} onClickMenu={onClickMenu} />
-            {menuIndex < menus.length - 1 && (
-              <div style={{ width: '100%', height: 1, backgroundColor: '#424242' }} />
+
+            {secondDepthMenus && (
+              <FlexColumn>
+                {secondDepthMenus.map((secondDepthMenu) => {
+                  if (!secondDepthMenu.display) {
+                    return null;
+                  }
+
+                  const isSelected = secondDepthMenu.router
+                    ? location.pathname.includes(secondDepthMenu.router)
+                    : false;
+
+                  return (
+                    <SidebarItem
+                      key={secondDepthMenu.router}
+                      isSelected={isSelected}
+                      menu={secondDepthMenu}
+                      onClickMenu={(childMenu) => {
+                        onClickSecondDepthMenu(menu, childMenu);
+                      }}
+                    />
+                  );
+                })}
+              </FlexColumn>
             )}
+
+            {menuIndex < menus.length - 1 && <Divider style={{ backgroundColor: '#424242' }} />}
           </FlexColumn>
         );
       })}
