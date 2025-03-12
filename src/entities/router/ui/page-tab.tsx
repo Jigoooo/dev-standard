a75bb7d8e3426c1ece895d4f9fdb5accadde57ed
Jigoooo <jiwook97@gmail.com>
2025-03-12 15:57:43 +0900

    동적 라우팅 수정

--- src/entities/router/ui/page-tab.tsx
+++ src/entities/router/ui/page-tab.tsx
@@ -54,7 +54,7 @@ export function PageTab({ aliveRef }: { aliveRef: RefObject<KeepAliveRef | undef
       return;
     }
 
-    navigate(menu.router);
+    navigate(menu.fullRouterPath);
   };
 
   return (
