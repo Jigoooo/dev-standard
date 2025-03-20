5436c1a96e3feb0f604ba5eec5ca3f49c33e51b7
Jigoooo <jiwook97@gmail.com>
2025-03-20 13:50:32 +0900

    셀렉트박스 수정

--- src/app/app.tsx
+++ src/app/app.tsx
@@ -8,6 +8,24 @@ import { preconnect } from 'react-dom';
 import { withRouterMenuHoc } from '@/app/providers';
 import { useRouterMenuContext } from '@/entities/router';
 
+if (import.meta.env.PROD) {
+  (function () {
+    const originalConsoleLog = console.log;
+    console.log = function (...args) {
+      if (
+        args.some(
+          (arg) =>
+            typeof arg === 'string' &&
+            (arg.includes('Unauthorized') || arg.includes('401') || arg.includes('403')),
+        )
+      ) {
+        return;
+      }
+      originalConsoleLog.apply(console, args);
+    };
+  })();
+}
+
 function App() {
   preconnect(import.meta.env.VITE_DEV_API_TARGET_URL);
 
