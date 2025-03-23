d43e60e1c2be123942890d7ac950290a0d5f3ab9
Jigoooo <jiwook97@gmail.com>
2025-03-23 22:48:59 +0900

    파일 업로드

--- src/pages/menu-setting/ui/menu-setting.tsx
+++ src/pages/menu-setting/ui/menu-setting.tsx
@@ -1,6 +1,13 @@
-import { FlexColumn, FlexRow, SaveButton } from '@/shared/ui';
+import { Reorder, useDragControls } from 'framer-motion';
+import { useState } from 'react';
+
+import { RxDragHandleDots2 } from 'react-icons/rx';
+
+import { FlexColumn, FlexRow, SaveButton, Typography } from '@/shared/ui';
 
 export function MenuSetting() {
+  const [items, setItems] = useState([0, 1, 2, 3]);
+
   return (
     <FlexColumn
       style={{
@@ -24,7 +31,50 @@ export function MenuSetting() {
         <SaveButton onClick={() => {}} />
       </FlexRow>
 
-      <FlexColumn></FlexColumn>
+      <Reorder.Group
+        axis='y'
+        values={items}
+        onReorder={setItems}
+        layoutScroll
+        style={{
+          userSelect: 'none',
+          overflowY: 'auto',
+          border: '1px solid #ccc',
+          borderRadius: 8,
+          maxHeight: 'calc(100vh - 300px)',
+          width: '50%',
+        }}
+      >
+        {items.map((item) => (
+          <ReorderItem key={item} item={item} />
+        ))}
+      </Reorder.Group>
     </FlexColumn>
+  );
+}
+
+function ReorderItem({ item }: { item: any }) {
+  const controls = useDragControls();
+
+  return (
+    <Reorder.Item
+      key={item}
+      value={item}
+      dragListener={false}
+      dragControls={controls}
+      style={{
+        display: 'flex',
+        alignItems: 'center',
+        width: '100%',
+        paddingInline: 12,
+        paddingBlock: 12,
+        gap: 4,
+      }}
+    >
+      <Typography>{item}</Typography>
+      <FlexRow onPointerDown={(e) => controls.start(e)} style={{ cursor: 'grab' }}>
+        <RxDragHandleDots2 />
+      </FlexRow>
+    </Reorder.Item>
   );
 }
