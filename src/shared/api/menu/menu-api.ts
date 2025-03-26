2ae6cb47713a9784b418d3cbbb1f32dfbfba6627
Jigoooo <jiwook97@gmail.com>
2025-03-26 15:03:51 +0900

    리팩토링

--- src/shared/api/menu/menu-api.ts
+++ src/shared/api/menu/menu-api.ts
@@ -1,9 +1,9 @@
 import { apiRequest, customedAxios, RMenu, RMenuList } from '@/shared/api';
 
 export async function getMenuListApi() {
-  return await apiRequest<RMenuList>(customedAxios.get('/v1/menus/info'));
+  return await apiRequest<RMenuList>(customedAxios.get('/v1/menus'));
 }
 
 export async function updateMenuApi(data: RMenu[]) {
-  return await apiRequest<null>(customedAxios.put('/v1/menus/info', data));
+  return await apiRequest<null>(customedAxios.put('/v1/menus', data));
 }
