ae7853098d3258df2cd115e7d9601c50a48bcea2
Jigoooo <jiwook97@gmail.com>
2025-04-09 10:06:53 +0900

    리팩토링

--- src/app/providers/theme/theme-provider.tsx
+++ src/app/providers/theme/theme-provider.tsx
@@ -1,17 +1,5 @@
-import { ReactNode, useEffect } from 'react';
+import type { ReactNode } from 'react';
 
 export function ThemeProvider({ children }: { children: ReactNode }) {
-  useEffect(() => {
-    const isWindows = navigator.userAgent.includes('Windows');
-
-    if (isWindows) {
-      document.documentElement.style.fontSize = '15px';
-    }
-
-    return () => {
-      document.documentElement.style.fontSize = '16px';
-    };
-  }, []);
-
   return <>{children}</>;
 }
